671d53c68f8242faec0c1d17cd6532e72cc4ec6c
Mutmahinat <Feyisara>
2024-12-05 23:09:24 -1200

    initial-commit

---
+++ question.js
@@ -0,0 +1,241 @@
+let questions = [
+  {
+  numb: 1,
+  question: "What is the basic unit of life?",
+  answer: "cell",
+  options: [
+    "Tissue",
+    "Cell",
+    "Organ",
+    "System"
+  ]
+},
+
+  {
+  numb: 2,
+  question: "What is the largest organ in the human body?",
+  answer: "Skin",
+  options: [
+    "Liver",
+    "Heart",
+    "Skin",
+    "Brain"
+  ]
+},
+
+  {
+  numb: 3,
+  question: "Which nutrient is essential for building strong bones?",
+  answer: "Calcium",
+  options: [
+    "Character",
+    "Calcium",
+    "String",
+    "Vitamin C"
+  ]
+},
+
+  {
+  numb: 4,
+  question: "What process do plants use to release water vapor?",
+  answer: "Transpiration",
+  options: [
+    "Fermentation",
+    "Respiration",
+    "Transpiration",
+    "Photosynthesis"
+  ]
+},
+
+  {
+  numb: 5,
+  question: "What is the largest planet in our solar system?",
+  answer: "Jupiter",
+  options: [
+    "Jupiter",
+    "Earth",
+    "Uranus",
+    "Saturn"
+  ]
+},
+
+{
+  numb: 6,
+  question: "Which chemical symbol represents gold?",
+  answer: "Au",
+  options: [
+    "Ag",
+    "Au",
+    "Fl",
+    "Cl"
+  ]
+},
+
+{
+  numb: 7,
+  question: "How do you create a function in JavaScript?",
+  answer: "function myFunction() {}",
+  options: [
+    "function:myFunction() {}",
+    "create myFunction() {}",
+    "function myFunction() {}",
+    "function = myFunction() {}"
+  ]
+},
+
+{
+  numb: 8,
+  question: "What type of energy is transferred through radiation?",
+  answer: "Electromagnetic",
+  options: [
+    "Electromagnetic",
+    "Mechanical",
+    "Electrical",
+    "Thermal"
+  ]
+},
+
+{
+  numb: 9,
+  question: "How can you add a comment in JavaScript?",
+  answer: "// This is a comment",
+  options: [
+    "<-- This is a comment -->",
+    "# This is a comment",
+    "// This is a comment",
+    "/ This is a comment"
+  ]
+},
+
+{
+  numb: 10,
+  question: "What is the output of Boolean(0)?",
+  answer: "false",
+  options: [
+    "false",
+    "0",
+    "undefined",
+    "true"
+  ]
+},
+
+{
+  numb: 11,
+  question: "What is the process of converting liquid to gas?",
+  answer: "Evaporation",
+  options: [
+    "boiling",
+    "Melting",
+    "Evaporation",
+    "Codensation"
+  ]
+},
+
+{
+  numb: 12,
+  question: "How do you declare a variable that cannot be reassigned?",
+  answer: "const",
+  options: [
+    "let",
+    "var",
+    "const",
+    "immutable"
+  ]
+},
+
+{
+  numb: 13,
+  question: "Who was the first president of the United States?",
+  answer: "George Washington",
+  options: [
+    "Thomas Jefferson",
+    "Abraham Lincoln",
+    "George Washington",
+    "Franklin D. Roosevelt"
+  ]
+},
+
+{
+  numb: 14,
+  question: "What will console.log('2' + 2) output?",
+  answer: "'22'",
+  options: [
+    "'22'",
+    "4",
+    "'4'",
+    "NaN"
+  ]
+},
+
+{
+  numb: 15,
+  question: "What does typeof [ ] return in JavaScript?",
+  answer: "'object'",
+  options: [
+    "'object'",
+    "'null'",
+    "'undefined'",
+    "'array'"
+  ]
+},
+
+{
+  numb: 16,
+  question: "Which statement is used to stop a loop?",
+  answer: "break",
+  options: [
+    "break",
+    "stop",
+    "exit",
+    "return"
+  ]
+},
+
+{
+  numb: 17,
+  question: "What is the process by which plants release oxygen?",
+  answer: "Photosynthesis",
+  options: [
+    "Decomposition",
+    "Photosynthesis",
+    "Fermentation",
+    "Respiration"
+  ]
+},
+
+{
+  numb: 18,
+  question: "Which animal group includes lions, tigers, and bears?",
+  answer: " Mammals",
+  options: [
+    "Carnivores",
+    "Herbivores",
+    "Omnivores",
+    " Mammals"
+  ]
+},
+
+{
+  numb: 19,
+  question: "What is the largest moon in our solar system?",
+  answer: "Ganymede",
+  options: [
+    "Titan",
+    "Europa",
+    "Ganymede",
+    "Callisto"
+  ]
+},
+
+{
+  numb: 20,
+  question: "What is the world's largest desert?",
+  answer: "Ganymede",
+  options: [
+    "Titan",
+    "Sahara",
+    "Mojave",
+    "Atacama "
+  ]
+},
+];
